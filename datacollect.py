3970da6ff55c567d339ea5c6aaf85fdf465f52ce
Boby Kurniawan <boby12kurniawan@gmail.com>
2022-08-29 22:02:17 +0700

    Finish

--- datacollect.py
+++ datacollect.py
@@ -10,28 +10,35 @@ count=0
 
 nameID=str(input("Enter Your Name: ")).lower()
 
-path='images/'+nameID
 
-isExist = os.path.exists(path)
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
-if isExist:
-	print("Name Already Taken")
-	nameID=str(input("Enter Your Name Again: "))
-else:
-	os.makedirs(path)
+assure_path_exists("images/")
+
 
 while True:
 	ret,frame=video.read()
+	frame = cv2.flip(frame,1)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 	if ret : 
-		faces=facedetect.detectMultiScale(frame,1.3, 5)
+		faces = facedetect.detectMultiScale(
+			gray,     
+			scaleFactor=1.2,
+			minNeighbors=5,     
+			minSize=(20, 20)
+        )
 		for x,y,w,h in faces:
 			count=count+1
-			name='./images/'+nameID+'/'+ str(count) + '.jpg'
+			name='images/'+nameID+'/'+ str(count) + '.jpg'
 			print("Creating Images........." +name)
-			cv2.imwrite(name, frame[y:y+h,x:x+w])
+			cv2.imwrite("images/user." + str(nameID) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+			# cv2.imwrite(name, frame[y:y+h,x:x+w])
 			cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
-		c
-		v2.imshow("WindowFrame", frame)
+		cv2.imshow("WindowFrame", frame)
 		cv2.waitKey(1)
 		if count>500:
 			break
--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-import tensorflow as tf
-from tensorflow import keras
 import numpy as np
 import cv2
-from keras.models import load_model
 import numpy as np
 
+names = [ 'Boby' , "WHO ?"]
 
 facedetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -14,32 +12,33 @@ cap.set(4, 480)
 font=cv2.FONT_HERSHEY_COMPLEX
 
 
-model = load_model('keras_model.h5')
 
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+recognizer.read('trainer/trainer.yml')
 
 def get_className(classNo):
 	return classNo
 
 while True:
 	sucess, imgOrignal=cap.read()
+	imgOrignal = cv2.flip(imgOrignal,1)
+
+	gray = cv2.cvtColor(imgOrignal,cv2.COLOR_BGR2GRAY)
+
 	faces = facedetect.detectMultiScale(imgOrignal,1.3,5)
 	for x,y,w,h in faces:
+
 		crop_img=imgOrignal[y:y+h,x:x+h]
 		img=cv2.resize(crop_img, (224,224))
 		img=img.reshape(1, 224, 224, 3)
-		prediction=model.predict(img)
-		classIndex=model.predict_step(img)
-		probabilityValue=np.amax(prediction)
-		if classIndex==0:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
-		elif classIndex==1:
-			cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
-			cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
-			cv2.putText(imgOrignal, str(get_className(classIndex)),(x,y-10), font, 0.75, (255,255,255),1, cv2.LINE_AA)
+		Id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+		cv2.rectangle(imgOrignal,(x,y),(x+w,y+h),(0,255,0),2)
+		cv2.rectangle(imgOrignal, (x,y-40),(x+w, y), (0,255,0),-2)
+		cv2.putText(imgOrignal, str(names[Id]),  (x,y-12), font,1, (255,0,0), 6)
+		cv2.putText(imgOrignal, str(" {0} %".format(round(100 - confidence))), (x-15,y+h-10), font, 1, (255,255,0), 1)
 
-		cv2.putText(imgOrignal,str(round(probabilityValue*100, 2))+"%" ,(180, 75), font, 0.75, (255,0,0),2, cv2.LINE_AA)
 	cv2.imshow("Result",imgOrignal)
 	k=cv2.waitKey(1)
 	if k==ord('q'):
